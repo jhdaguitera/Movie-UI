691752ebfdd6200a3a32d1d8846595077f29ab75
JHD <45046656+jhdaguitera@users.noreply.github.com>
2020-11-08 20:58:21 -0800

    Add files via upload

--- java.js
+++ java.js
@@ -7,33 +7,86 @@ function carousel() {
     slideshow = document.getElementsByClassName("carouselSlides");
     //iterate through slideshow and count length
     for (i = 0; i < slideshow.length; i++) {
-       slideshow[i].style.display = "none";  
+        slideshow[i].style.display = "none";
     }
     slidePos++;
     //start slideshow back at 1 if over
-    if (slidePos > slideshow.length) {slidePos = 1}    
-    slideshow[slidePos - 1].style.display = "block"; 
+    if (slidePos > slideshow.length) { slidePos = 1 }
+    slideshow[slidePos - 1].style.display = "block";
     //change carousel image every 3 seconds 
-    setTimeout(carousel, 2000); 
+    setTimeout(carousel, 2000);
 }
 
 function currentSlide(index) {
-    if (index > slideshow.length) {index = 1}
+    if (index > slideshow.length) { index = 1 }
     //keep track of index of current slides..same as above
-    else if (index < 1){index = slideshow.length}
+    else if (index < 1) { index = slideshow.length }
     for (i = 0; i < slideshow.length; i++) {
-       slideshow[i].style.display = "none";  
+        slideshow[i].style.display = "none";
     }
-    slideshow[index-1].style.display = "block";  
+    slideshow[index - 1].style.display = "block";
 }
 
 function carouselPos(position) {
     slidePos += position;
     //keep track of where in carousel is current slide
-    if (slidePos > slideshow.length) {slidePos = 1}
-    else if(slidePos < 1){slidePos = slideshow.length}
+    if (slidePos > slideshow.length) { slidePos = 1 } else if (slidePos < 1) { slidePos = slideshow.length }
     for (i = 0; i < slideshow.length; i++) {
-       slideshow[i].style.display = "none";  
+        slideshow[i].style.display = "none";
     }
-    slideshow[slidePos - 1].style.display = "block";  
+    slideshow[slidePos - 1].style.display = "block";
+}
+
+
+
+//adding selection item to the Search Movies display table
+function getSelectedDate() {
+    var e = document.getElementById("date");
+    var result = e.options[e.selectedIndex].text;
+
+    document.getElementById("resultDate").innerHTML = result;
+}
+
+function getSelectedTime() {
+    var e = document.getElementById("times");
+    var result = e.options[e.selectedIndex].text;
+
+    document.getElementById("resultTime").innerHTML = result;
+}
+
+function getSelectedMovie() {
+    var e = document.getElementById("movies");
+    var result = e.options[e.selectedIndex].text;
+
+    document.getElementById("resultMovie").innerHTML = result;
+}
+
+
+//adding selection item to the Purchase Tickets display table
+function getSelectedAdult() {
+    var e = document.getElementById("adults");
+    var result = e.options[e.selectedIndex].text;
+
+    document.getElementById("resultAdult").innerHTML = result;
+}
+
+function getSelectedKid() {
+    var e = document.getElementById("children");
+    var result = e.options[e.selectedIndex].text;
+
+    document.getElementById("resultKid").innerHTML = result;
+}
+
+function getSelectedSr() {
+    var e = document.getElementById("seniors");
+    var result = e.options[e.selectedIndex].text;
+
+    document.getElementById("resultSr").innerHTML = result;
+}
+
+//adding inputs into Transaction table
+
+function getInputAdult() {
+    var inputVal = document.getElementById("fname").text;
+    alert(inputVal);
 }
